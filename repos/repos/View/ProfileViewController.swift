c44976ad4e6fa7f1fa55b5821525daeb447eb438
Jenn <paskua4160@gmail.com>
2018-08-31 14:53:27 +0900

    User api 호출해서 사용자 정보 받아온 후, 화면에 사용자 정보 업데이트해줍니다. 스크롤 다운 시 Pull To Refresh 되도록 추가하였습니다.

--- repos/repos/View/ProfileViewController.swift
+++ repos/repos/View/ProfileViewController.swift
@@ -7,9 +7,12 @@
 //
 
 import UIKit
+import Kingfisher
+import Alamofire
 
 final class ProfileViewController: UIViewController {
-
+    let username = "JennPaskua4160"
+    
     @IBOutlet var userThumbnail: UIImageView!
     @IBOutlet var userName: UILabel!
     @IBOutlet var address: UILabel!
@@ -21,10 +24,76 @@ final class ProfileViewController: UIViewController {
     @IBOutlet var followers: UILabel!
     @IBOutlet var followings: UILabel!
     
+    @IBOutlet var scrollView: UIScrollView!
+    var refreshControl: UIRefreshControl!
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
-        //Todos: user api 부르기
+        super.viewDidLoad()        
+        self.setupPullToRefresh()
+        
+        //TODO: user Api 요청할 때 UI그리는 속도개선필요.
+        self.requestUserInfo(username: self.username) { [weak self] (data) in
+            guard let `self` = self else { return }
+            guard let userData = data else { return }
+            self.updateUI(userData: userData)
+        }
     }
-
-
+    
+    //Pull To Refresh 생성 함수
+    func setupPullToRefresh() {
+        refreshControl = UIRefreshControl()
+        refreshControl.addTarget(self, action: #selector(didPullToRefresh), for: .valueChanged)
+        self.scrollView.addSubview(refreshControl)
+    }
+    
+    //User 정보 리프레시해주는 부분
+    @objc func didPullToRefresh() {
+        self.requestUserInfo(username: self.username) { [weak self] (data) in
+            guard let `self` = self else { return }
+            guard let userData = data else { return }
+            self.updateUI(userData: userData)
+        }
+        refreshControl?.endRefreshing()
+    }
+    
+    
+    //User 정보 화면에 그려주는 부분
+    func updateUI(userData: User) {
+        self.userThumbnail.layer.borderColor = UIColor.darkGray.cgColor
+        self.userThumbnail.layer.cornerRadius = self.userThumbnail.frame.height/2
+        self.userThumbnail.clipsToBounds = true
+        self.userThumbnail.kf.setImage(with: userData.avatarURL)
+        
+        self.userName.text = userData.name ?? ""
+        self.address.text = userData.location ?? ""
+        self.blog.text = userData.blog ?? ""
+        self.email.text = userData.email ?? ""
+        self.repositories.text = "\(userData.repos)"
+        self.stars.text = "\(userData.stars)"
+        self.followers.text = "\(userData.followers)"
+        self.followings.text = "\(userData.following)"
+    }
+    
+    // MARK: - User api 호출하는 함수
+    func requestUserInfo(username: String, completion: @escaping (User?) -> Void) {
+        
+        request(Router.user(username)).responseJSON { response in
+            guard response.result.isSuccess,
+                let _ = response.result.value else {
+                    print("Error while fetching userList: \(String(describing: response.result.error))")
+                    completion(nil)
+                    return
+            }
+            
+            let jsonData = response.data
+            
+            do{
+                let JSON = try JSONDecoder().decode(User.self, from: jsonData!)
+                completion(JSON)
+            } catch{
+                print("error \(error)")
+                completion(nil)
+            }
+        }
+    }
 }

fb18e02b36eb2cc234c8adb03422113f68323df1
Jenn <paskua4160@gmail.com>
2018-08-31 16:59:34 +0900

    SearchViewController에 익스텐션으로 UISearchBarDelegate 추가하였습니다. 키보드 done버튼 누르거나, 화면을 탭하였을 때 키보드가 사라지도록 합니다.

--- repos/repos/View/SearchViewController.swift
+++ repos/repos/View/SearchViewController.swift
@@ -20,7 +20,27 @@ final class SearchViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.createSearchBar()
+        self.hideKeyboardWhenTappedAround()
         
+        //Todo: 현재 알파벳 하나씩 Repository api 호출하여 테이블뷰 갱신하도록 구현
+        self.searchBar
+            .rx
+            .text
+            .orEmpty
+            .debounce(0.5, scheduler: MainScheduler.instance)
+            .distinctUntilChanged()
+            .subscribe(onNext: { [weak self] query in
+                guard let `self` = self else { return }
+                //respositories 함수 호출 필요
+            })
+    }
+    
+    //SearchBar 만드는 함수
+    func createSearchBar() {
+        self.searchBar.delegate = self
+        self.searchBar.returnKeyType = UIReturnKeyType.done
+        self.searchBar.placeholder = "Search repository issue"
     }
 }
 
@@ -31,9 +51,26 @@ extension SearchViewController: UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ReposCellID", for: indexPath) as! ReposCell
-
+        
         return cell
     }
+}
+
+// MARK: - UISearchBar 델리게이트 추가
+extension SearchViewController: UISearchBarDelegate {
+    //done버튼 눌렀을 때 키보드 사라지게 하는 함수
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        self.searchBar.resignFirstResponder()
+    }
     
+    //화면 탭하면 키보드 사라지게 하는 함수
+    func hideKeyboardWhenTappedAround() {
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action:    #selector(SearchViewController.dismissKeyboard))
+        tap.cancelsTouchesInView = false
+        view.addGestureRecognizer(tap)
+    }
     
+    @objc func dismissKeyboard() {
+        self.searchBar.resignFirstResponder()
+    }
 }

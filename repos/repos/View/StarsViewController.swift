21301c186903ed42f1c33d7e904768fb6d6981cb
Jenn <paskua4160@gmail.com>
2018-09-01 21:28:50 +0900

    StarsViewController와 StarsCell을 생성하였습니다. 현재 유저의 Starred repository 리스트 목록을 화면에 보여줍니다. (PullToRefresh기능 추가하였고 통신부와 로직은 SearchViewController와 동일합니다.)

---
+++ repos/repos/View/StarsViewController.swift
@@ -0,0 +1,97 @@
+//
+//  StarsViewController.swift
+//  repos
+//
+//  Created by Jean on 2018. 9. 1..
+//  Copyright © 2018년 com.paskua.swift. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+
+final class StarsViewController: UIViewController {
+    
+    @IBOutlet var table: UITableView!
+    let username: String = "JennPaskua4160"
+    var totalCount: Int = 0
+    var pageId: Int = 1
+    var repos: [Repo] = [Repo]()
+    var refreshControl: UIRefreshControl!
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+        self.updateAfterGetStarredRepo()
+    }
+    
+    //Pull To Refresh 생성 함수
+    func setupPullToRefresh() {
+        refreshControl = UIRefreshControl()
+        refreshControl.addTarget(self, action: #selector(didPullToRefresh), for: .valueChanged)
+        self.table.addSubview(refreshControl)
+    }
+    
+    //User 정보 리프레시해주는 부분
+    @objc func didPullToRefresh() {
+        self.pageId = 1
+        self.updateAfterGetStarredRepo()
+        refreshControl?.endRefreshing()
+    }
+    
+    func updateAfterGetStarredRepo() {
+        self.fetchMyStarredRepo(pageId: self.pageId) { [weak self] (data) in
+            guard let `self` = self else { return }
+            guard let reposData = data else { return }
+            
+            self.repos = reposData
+            self.table.reloadData()
+        }
+    }
+    
+    // MARK: - starred repository list api
+    func fetchMyStarredRepo(pageId: Int, completion: @escaping ([Repo]?) -> Void) {
+        
+        request(Router.starredRepository(username: self.username, pageId: pageId)).responseJSON { response in
+            guard response.result.isSuccess,
+                let _ = response.result.value else {
+                    print("Error while fetching repository: \(String(describing: response.result.error))")
+                    completion(nil)
+                    return
+            }
+            
+            let jsonData = response.data
+            
+            do {
+                let JSON = try JSONDecoder().decode([Repo].self, from: jsonData!)
+                completion(JSON)
+            }catch {
+                print("Error while fetching JSONDecoding\(error)")
+                completion(nil)
+            }
+        }
+    }
+}
+
+// MARK: - UITableViewDelegate, UITableViewDataSource
+extension StarsViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        guard !self.repos.isEmpty else { return 0 }
+        return self.repos.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "StarsCellID", for: indexPath) as? StarsCell else { return UITableViewCell() }
+        guard !self.repos.isEmpty else { return UITableViewCell() }
+        
+        cell.configure(cellData: self.repos[indexPath.row])
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 50
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return UITableViewAutomaticDimension
+    }
+}
---
+++ repos/repos/View/cell/StarsCell.swift
@@ -0,0 +1,26 @@
+//
+//  StarsCell.swift
+//  repos
+//
+//  Created by Jean on 2018. 9. 1..
+//  Copyright © 2018년 com.paskua.swift. All rights reserved.
+//
+
+import UIKit
+
+final class StarsCell: UITableViewCell {
+    @IBOutlet var repoTitle: UILabel!
+    @IBOutlet var repoSubTitle: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        self.repoTitle.text = ""
+        self.repoSubTitle.text = ""
+    }
+    
+    func configure(cellData: Repo) {
+        self.repoTitle.text = cellData.repoFullName ?? ""
+        self.repoSubTitle.text = cellData.description ?? ""
+    }
+    
+}

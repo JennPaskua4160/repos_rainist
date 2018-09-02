d9119bab43c9a8981f0b46ef392ec101dfcf6566
Jenn <paskua4160@gmail.com>
2018-09-02 22:29:52 +0900

    DetailViewController에 getRepos api 함수 추가하여 starCount 업데이트해줍니다. 버튼 클릭 시 바로 카운트 1 증감하도록 구성하였습니다.

--- repos/repos/View/DetailViewController.swift
+++ repos/repos/View/DetailViewController.swift
@@ -16,12 +16,12 @@ final class DetailViewController: UIViewController {
     @IBOutlet var starsCountTitle: UILabel!
     @IBOutlet var starsButton: UIButton!
     
-    var reposCount: Int = 0 {
+    var starCount: Int = 0 {
         didSet {
-            self.starsCountTitle.text = "\(reposCount)"
+            self.starsCountTitle.text = "\(starCount)"
         }
     }
-    
+
     var detailRepo: Repo? {
         didSet {
             configureView()
@@ -38,7 +38,7 @@ final class DetailViewController: UIViewController {
             repoTitle.text = detailRepo.repoFullName
             repoSubTitle.text = detailRepo.description
             starsCountTitle.text = "\(detailRepo.starsCount)"
-            self.reposCount = detailRepo.starsCount
+            
             title = detailRepo.repoFullName
         }
     }
@@ -51,19 +51,43 @@ final class DetailViewController: UIViewController {
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(true)
         self.checkIfStarredRepos()
+        self.getRepos { (data) in
+            self.starCount = data?.starsCount ?? 0
+        }
     }
     
     @IBAction func starsButtonClicked(_ sender: UIButton) {
         sender.isSelected = !sender.isSelected
         
         if sender.isSelected {
-            self.starsButton.isSelected = true
             self.putStars()
-            self.reposCount = self.reposCount + 1
         } else {
-            self.starsButton.isSelected = false
             self.deleteStars()
-            self.reposCount = self.reposCount - 1
+        }
+    }
+    
+    func getRepos(completion: @escaping (Repo?) -> Void ) {
+        guard let detailRepo = detailRepo else { return }
+        
+        request(Router.getRepos(owner: detailRepo.owner.login,
+                                repo: detailRepo.repoName)).responseJSON { response in
+                                    
+                                    guard response.result.isSuccess,
+                                        let _ = response.result.value else {
+                                            print("Error while fetching repository: \(String(describing: response.result.error))")
+                                            completion(nil)
+                                            return
+                                    }
+                                    
+                                    let jsonData = response.data
+                                    
+                                    do {
+                                        let JSON = try JSONDecoder().decode(Repo.self, from: jsonData!)
+                                        completion(JSON)
+                                    }catch {
+                                        print("Error while fetching JSONDecoding\(error)")
+                                        completion(nil)
+                                    }
         }
     }
     
@@ -91,13 +115,17 @@ final class DetailViewController: UIViewController {
     func putStars() {
         
         request(Router.star(owner: self.detailRepo?.owner.login ?? "",
-                            repo: self.detailRepo?.repoName ?? "")).responseJSON { response in
+                            repo: self.detailRepo?.repoName ?? "")).response { response in
                                 
-                                guard response.result.error == nil else {
-                                    print("error calling PUT on current repo")
-                                    if let error = response.result.error {
-                                        print("Error: \(error)")
-                                    }
+                                switch (response.response?.statusCode) {
+                                case 204:
+                                    //화면에 바로 카운팅이 증가하도록 수정
+                                    //TODO: back button 눌렀다가 돌아왔을 때 starCount 5초 후 갱신되고 있음.
+                                    self.starCount = self.starCount + 1
+                                case 404:
+                                    print("error while put star")
+                                    return
+                                case .none, .some(_):
                                     return
                                 }
         }
@@ -108,13 +136,18 @@ final class DetailViewController: UIViewController {
     func deleteStars() {
         
         request(Router.unStar(owner: self.detailRepo?.owner.login ?? "",
-                              repo: self.detailRepo?.repoName ?? "")).responseJSON { response in
+                              repo: self.detailRepo?.repoName ?? "")).response { response in
                                 
-                                guard response.result.error == nil else {
-                                    print("error calling DELETE on current repo")
-                                    if let error = response.result.error {
-                                        print("Error: \(error)")
-                                    }
+                                switch (response.response?.statusCode) {
+                                case 204:
+                                    //화면에 바로 카운팅이 증가하도록 수정
+                                    //TODO: back button 눌렀다가 돌아왔을 때 starCount 5초 후 갱신되고 있음.
+                                    self.starCount = self.starCount - 1
+                                    return
+                                case 404:
+                                    print("error while delete star")
+                                    return
+                                case .none, .some(_):
                                     return
                                 }
         }

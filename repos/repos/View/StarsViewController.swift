bac286feb673897420e8375a5d8b13acfbec4ac7
Jenn <paskua4160@gmail.com>
2018-09-02 16:59:47 +0900

    StarsViewController 수정

--- repos/repos/View/StarsViewController.swift
+++ repos/repos/View/StarsViewController.swift
@@ -13,10 +13,14 @@ final class StarsViewController: UIViewController {
     
     @IBOutlet var table: UITableView!
     let username: String = "JennPaskua4160"
-    var totalCount: Int = 0
     var pageId: Int = 1
     var repos: [Repo] = [Repo]()
     var refreshControl: UIRefreshControl!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.setupPullToRefresh()
+    }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(true)
@@ -30,27 +34,29 @@ final class StarsViewController: UIViewController {
         self.table.addSubview(refreshControl)
     }
     
-    //User 정보 리프레시해주는 부분
+    //Repo 정보 리프레시해주는 부분
     @objc func didPullToRefresh() {
         self.pageId = 1
+        self.repos.removeAll()
         self.updateAfterGetStarredRepo()
         refreshControl?.endRefreshing()
     }
     
     func updateAfterGetStarredRepo() {
-        self.fetchMyStarredRepo(pageId: self.pageId) { [weak self] (data) in
+        self.fetchMyStarredRepo() { [weak self] (data) in
             guard let `self` = self else { return }
             guard let reposData = data else { return }
             
             self.repos = reposData
+            self.title = "My Starred Repos [\(self.repos.count)]"
             self.table.reloadData()
         }
     }
     
     // MARK: - starred repository list api
-    func fetchMyStarredRepo(pageId: Int, completion: @escaping ([Repo]?) -> Void) {
+    func fetchMyStarredRepo(completion: @escaping ([Repo]?) -> Void) {
         
-        request(Router.starredRepository(username: self.username, pageId: pageId)).responseJSON { response in
+        request(Router.starredRepository(username: self.username, pageId: self.pageId)).responseJSON { response in
             guard response.result.isSuccess,
                 let _ = response.result.value else {
                     print("Error while fetching repository: \(String(describing: response.result.error))")
